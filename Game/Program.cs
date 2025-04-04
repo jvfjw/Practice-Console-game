ae3914737bddeb11e965be4fadaadfb5088fa849
João Ferreira <joaovfsilva35@gmail.com>
2025-04-04 10:48:04 -0300

    Added colors!

--- Game/Program.cs
+++ Game/Program.cs
@@ -7,8 +7,6 @@
         int level = 1;
         while (menuRunning)
         {
-           
-            Console.ForegroundColor = ConsoleColor.White;
             bool gameRunning = true;
             Random randomNumber = new Random();
             int multiplier;
@@ -18,6 +16,7 @@
 
 
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Welcome to the guess the number game!");
             Console.WriteLine($"You are on level {level}!");
 
@@ -34,19 +33,28 @@
                     if (userInputInt < numberToGuess)
                     {
                         Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
                         score -= 20;
-                        Console.WriteLine($"YOUR GUESS IS TOO LOW!\nYour score now is: {score}");
+                        Console.WriteLine($"YOUR GUESS IS TOO LOW!\n");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine($"Your score now is: {score}");
                     }
                     else if (userInputInt > numberToGuess)
                     {
                         Console.Clear();
-                        Console.WriteLine($"YOUR GUESS IS TOO HIGH!\nYour score now is: {score}");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"YOUR GUESS IS TOO HIGH!\n");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine($"Your score now is: {score}");
                     }
                     else if (userInputInt == numberToGuess)
                     {
                         Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
                         score += 200;
-                        Console.WriteLine($"YOU GUESSED IT RIGHT!\nYour score now is: {score}");
+                        Console.WriteLine($"YOU GUESSED IT RIGHT!\n");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine($"Your score now is: {score}");
                         Console.WriteLine($"Do you want to go to level {level + 1}?   (y/n)");
                         string answer = Console.ReadLine();
 
